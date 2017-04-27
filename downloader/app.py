3bce8ed5985484818796b9162be5d28dccc5fdeb
Lini Mestar <lini.mestar@jpl.nasa.gov>
2017-04-26 20:42:53 -0700

    specify Youtube to only search videos

--- downloader/app.py
+++ downloader/app.py
@@ -8,18 +8,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def downloader():
-#    try:
-    for vid in BeautifulSoup(urllib2.urlopen\
-        ("https://www.youtube.com/results?search_query=" + \
-        urllib.quote(json.dumps(request.json['url'])) + \
-        "audio").read()).findAll(attrs={'class':'yt-uix-tile-link'}):
-        url= 'https://www.youtube.com' + vid['href']
-        break
-    subprocess.call("youtube-dl -x --embed-thumbnail --audio-format mp3 -o '/src/tmp/%(title)s.%(ext)s' " +url, shell=True)
-    subprocess.call("sh upload.sh", shell=True)
-    return "Got "+vid['title']+'.mp3'
-#    except:
-#        print "Couldn't get a song."
+    try:
+        #Get only videos
+        for vid in BeautifulSoup(urllib2.urlopen\
+            ("https://www.youtube.com/results?sp=EgIQAQ%253D%253D&q=" + \
+            urllib.quote(json.dumps(request.json['url'])) + \
+            "audio").read()).findAll(attrs={'class':'yt-uix-tile-link'}):
+            url= 'https://www.youtube.com' + vid['href']
+            break
+    except:
+        return "Wasn't able to reach youtube."
+    try:
+        #Download top result
+        subprocess.call("youtube-dl -x --audio-format mp3 -o '/src/tmp/%(title)s.%(ext)s' " +url, shell=True)
+        subprocess.call("sh upload.sh", shell=True)
+        return "Got "+vid['title']+'.mp3'
+    except:
+        return "Youtube Downloader failed."
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port='5010')
