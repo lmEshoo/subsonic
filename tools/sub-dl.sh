f377e009d6a2b394b0d890b530757cbd3e07205f
Lini Mestar <lini.mestar@jpl.nasa.gov>
2017-06-19 01:42:46 -0700

    save money on pulling downfrom s3

---
+++ tools/sub-dl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $1 = "up" ]; then
+  cd downloader && make run
+elif [ $1 = "rec" ]; then
+  cd recommender && make run
+elif [ $1 = "post" ]; then
+  #post processing
+  i=1; sp="/-\|";
+  cp /app/tools/setSubCoverArt.py /var/music/;
+  cp /app/tools/setTags.py /var/music/;
+  sleep 1;
+  while [[ ! `curl -sf http://$INSTANCE_IP:4040/login.view?` ]]; do sleep 5; done
+  cd /var/music/ && python setSubCoverArt.py
+  cd /var/music/ && python setTags.py
+  cd /var/music/ && find . -name "*.mp3" -size -2000k -delete
+  cd /app/tools/ && python refresh.py
+else
+  cd downloader && make clean
+  cd ../recommender && make clean
+  kill -9 `ps -aux | grep python | grep -v grep | awk '{ print $2 }'`
+fi
