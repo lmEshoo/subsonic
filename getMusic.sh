75660b1a26e8b3d1267aa2e539aee2b8d5ca45b9
Lini Mestar <lini.mestar@jpl.nasa.gov>
2017-06-19 01:19:40 -0700

    structured vs unstructed music

--- getMusic.sh
+++ getMusic.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #s3 bucket contains all my music called drake-more-life
-
-aws s3 sync s3://drake-more-life /var/music
+rm -r /var/music/*
+aws s3 sync s3://drake-more-life /var/s3
+cp /var/s3/* /var/music/
